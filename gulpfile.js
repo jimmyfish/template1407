07487ab2a93dbd545b89cafce53f67f087b7e4d8
Prasetyo Wicaksono <pras@openmailbox.org>
2017-05-31 00:14:17 +0700

    configuring gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    minifyCSS = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    runSequence = require('run-sequence');
+
+gulp.task('default', function() {
+    runSequence(
+        'semantic'
+    );
+});
+
+gulp.task('semantic', function() {
+    runSequence(
+        'semantic-base',
+        'semantic-theme'
+    );
+});
+
+gulp.task('semantic-base', function() {
+    gulp.dest('public/dist/semantic/semantic.less')
+    .pipe(less())
+    .pipe(concat(./web/css/semantic.min.css))
+    .pipe(autoprefixer())
+    .pipe(minifyCSS({
+        keepSpecialComments: false
+    }))
+});
